048035d223e22aea7704cb61fad7a91eabdd9934
maksim-maslov <brat.maksim@gmail.com>
2017-07-24 19:23:50 +0300

    task 1

--- homework-1.3.php
+++ homework-1.3.php
@@ -1,4 +1,10 @@
 <?php
+$multiArray = [
+    'Africa' => ['Vulpes pallida', 'Herpestes ichneumon', 'Atelerix algirus', 'Loxodonta', 'Felis margarita'],
+    'Australia' => ['Pelecanus conspicillatus', 'Sarcophilus harrisii', 'Zaglossus bruijni', 'Macropus agilis', 'Phascolarctos cinereus'],
+    'Eurasia' => ['Tamias sibiricus', 'Mus spicilegus', 'Talpa caucasica', 'Bison', 'Castor fiber']
+];
+
 
 
 ?>
