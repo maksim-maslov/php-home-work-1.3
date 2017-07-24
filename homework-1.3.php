a0215a91fc345fbf8262466ca3153480050a89e9
maksim-maslov <brat.maksim@gmail.com>
2017-07-24 22:17:42 +0300

    task 3

--- homework-1.3.php
+++ homework-1.3.php
@@ -1,18 +1,48 @@
 <?php
-$multiArray = [
+$multiArrays = [
     'Africa' => ['Vulpes pallida', 'Herpestes ichneumon', 'Atelerix algirus', 'Loxodonta', 'Felis margarita'],
     'Australia' => ['Pelecanus conspicillatus', 'Sarcophilus harrisii', 'Zaglossus bruijni', 'Macropus agilis', 'Phascolarctos cinereus'],
     'Eurasia' => ['Tamias sibiricus', 'Mus spicilegus', 'Talpa caucasica', 'Bison', 'Castor fiber']
 ];
 
-$twoWordsArrays = [];
+$twoWordsArray = [];
 
-foreach ($multiArray as $continent => $animal) {
+foreach ($multiArrays as $continent => $animal) {
     foreach ($animal as $animalName) {
         if (substr_count ($animalName, ' ') > 0) {
-        	$twoWordsArrays[] = $animalName;
+        	$twoWordsArray[] = $animalName;
         }
     }
+}
+
+$twoWordsArrayShuffle = $twoWordsArray;
+shuffle($twoWordsArrayShuffle);
+
+$i = 0;
+foreach ($twoWordsArrayShuffle as $item) {
+	$itemsArrayShuffle = explode(' ', $item);
+	$itemstwoWordsArray = explode(' ', $twoWordsArray[$i]);
+	$twoWordsArray[$i] = str_replace($itemstwoWordsArray[1], $itemsArrayShuffle[1], $twoWordsArray[$i]);	
+	$i++;
+}
+
+foreach ($multiArrays as $continent => $animal) {
+
+	?>
+
+	<h2><?= $continent ?></h2>
+
+	<?php
+
+	$outputArray = [];
+	foreach ($twoWordsArray as $twoWordsArrayItem) {
+		foreach ($animal as $animalName) {			
+	        if (substr_count ($animalName, substr($twoWordsArrayItem, 0, strpos($twoWordsArrayItem, ' ') - 1)) > 0) {
+	        	$outputArray [] = $twoWordsArrayItem;
+	        }
+	    }		
+	}
+	echo implode(', ', $outputArray);
 }
 
 ?>
